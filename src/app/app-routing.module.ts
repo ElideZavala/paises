80fa188f732bef2f84d6e5ed24752185531ece04
ElideZavala <zavalavinagreelide@gmail.com>
2022-10-02 20:38:28 -0500

    sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,42 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
 
+// Definimos cada uno de mis rutas
 const routes: Routes = [
   {
     path: '',
-    component: PorPaisComponent,
-    pathMatch: 'full'
+    component: PorPaisComponent, // Tiene que esta importado en el app.module.ts y exportado.
+    pathMatch: 'full' // Para que sea la ruta por defecto
   },
   {
     path: 'region',
-    component: PorRegionComponent
+    component: PorRegionComponent,
   },
   {
     path: 'capital',
-    component: PorCapitalComponent
+    component: PorCapitalComponent,
   },
   {
     path: 'pais/:id',
-    component: VerPaisComponent
+    component: VerPaisComponent,
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: '' // Si no encuentra la ruta, redirecciona a la ruta por defecto
   }
-];
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes) // ForRoot es para rutas principales
+  ],
+  exports: [
+    RouterModule // Para que se pueda usar en otros modulos
+  ]
 })
+
 export class AppRoutingModule { }
